3504bf96e4d631e1f651e68401b9755c598e3a9a
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-09 18:52:05 -0300

    more

--- AULA_9_Encapsulamento.py
+++ AULA_9_Encapsulamento.py
@@ -16,13 +16,25 @@ class foo:
     def __init__(self):
         self.public = 'isso é público'
         self._protected = 'isso é protegido'
+        self.__private = 'isso é privado'
 
     def metodo_publico(self):
         return 'método público'
 
+    def _metodo_protegido(self):
+        return 'método protegido'
+
+    def __metodo_privado(self):
+        return 'método privado'
 
 f = foo()
 print(f.public)
 print(f.metodo_publico())
 
-print(f._protected)
+print(f._protected)           # -> Errado
+print(f._metodo_protegido())  # -> Errado
+
+
+# O nome do método foi alterado por ser um método privado, só será o original dentro da classe original
+print(f.__private)            # -> Errado
+print(f.__metodo_privado())   # -> Errado
