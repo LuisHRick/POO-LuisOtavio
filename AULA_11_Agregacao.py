79a0ae7d6a52e8d4e34645ec3a5759850a88cc22
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-10 14:33:49 -0300

    more

--- AULA_11_Agregacao.py
+++ AULA_11_Agregacao.py
@@ -8,3 +8,35 @@
 # se tratar de uma relação onde um objeto precisa de
 # outro para fazer determinada tarefa.
 # (existem controvérsias sobre as definições de agregação).
+
+class Carrinho:
+    def __init__(self) -> None:
+        self._produtos = []
+
+
+    def inserir_produtos(self, *produtos):
+        #for produto in produtos:
+        #    self._produtos.append(produto)
+        self._produtos.extend(produtos)
+
+
+    def total(self):
+        return sum(p.preco for p in self._produtos)
+
+    def listar_produtos(self):
+        print()
+        for produto in self._produtos:
+            print(produto.nome, produto.preco)
+        print()
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+carrinho = Carrinho()
+p1, p2 = Produto('CANETA', 1.20), Produto('CAMISETA', 30.5)
+
+carrinho.inserir_produtos(p1, p2)
+carrinho.listar_produtos()
+print(carrinho.total())
