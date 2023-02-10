44077f29028ad35549a7b84859367676c340a7ef
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-10 13:35:41 -0300

    more

---
+++ AULA_10_Associacao.py
@@ -0,0 +1,36 @@
+# Relações entre classes: associação, agregação e composição
+# Associação é um tipo de relação onde os objetos
+# estão ligados dentro do sistema.
+# Essa é a relação mais comum entre objetos e tem subconjuntos
+# como agregação e composição (que veremos depois).
+# Geralmente, temos uma associação quando um objeto tem
+# um atributo que referencia outro objeto.
+# A associação não especifica como um objeto controla
+# o ciclo de vida de outro objeto.
+
+class Escritor:
+    def __init__(self, nome):
+        self.nome = nome
+        self._ferramenta = None
+
+    @property
+    def ferramenta(self):
+        return self._ferramenta
+
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        return self._ferramenta
+
+class Ferramenta:
+    def __init__(self, nome):
+        self.nome = nome
+    
+    def escrever(self):
+        return f'{self.nome} está escrevendo'
+
+
+
+ESCRITOR = Escritor('Luis')
+FERRAMENTA = Ferramenta('BIC')
+
+ESCRITOR.ferramenta = 'Caneta'
