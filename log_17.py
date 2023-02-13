d8ab2240fa414d00a35ccb7e6a7f6f3370ed7a34
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-13 15:26:05 -0300

    more

--- log_17.py
+++ log_17.py
@@ -1,14 +1,21 @@
 # Abstração
 # log
 class Log:
-    def log(self,msg):
+    def _log(self,msg):
         raise NotImplementedError(
             'Implemente o método log'
         )
 
+    def log_error(self,msg):
+        self._log(f'Error: {msg}')
+
+    def log_success(self,msg):
+        self._log(f'Success: {msg}')
+
+
 class LogFileMixin(Log):
 
-    def log(self, msg):
+    def _log(self, msg):
         print(msg)
 
 if __name__ == '__main__':
