ae70aae1d7f7b51ba5e60d98009e0072d9858143
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-14 17:14:53 -0300

    more

--- AULA_19_Abstração.py
+++ AULA_19_Abstração.py
@@ -20,7 +20,7 @@ from abc import ABC, abstractmethod
 class Log(ABC):
 
     @abstractmethod
-    def _log(self,msg): ...
+    def _log(self,msg):  ...
 
     def log_error(self,msg):
         self._log(f'Error: {msg}')
