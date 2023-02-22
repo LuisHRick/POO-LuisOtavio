7a7cf8643df8ddcaad5fd01357699e3e6fe9f5e6
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-22 13:31:49 -0300

    more

---
+++ Aula 30/AULA_30_Docstring.py
@@ -0,0 +1,12 @@
+import uma_linha
+import varias_linhas
+
+# print(dir(uma_linha)) # mostra o que tem lá
+
+# print(uma_linha.__dict__)
+# print(uma_linha.__name__)
+# print(uma_linha.func)
+
+# help(uma_linha) # mostra tudo do arquivo
+
+help(varias_linhas)
---
+++ Aula 30/varias_linhas.py
@@ -0,0 +1,74 @@
+""" DESCRIÇÃO
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elementum, ligula 
+in ultricies placerat, risus erat ultrices 
+est, finibus faucibus nibh leo non magna. Fusce scelerisque accumsan elementum. 
+Sed interdum, lectus quis pretium semper, 
+odio purus convallis ante, id mollis leo nibh porta nibh. Morbi sem ipsum, bland
+it sed posuere ut, condimentum vel ex. Quisque 
+dapibus ligula quis dolor dignissim scelerisque. Duis nec ornare neque, non ultr
+icies lectus. Nulla imperdiet ultricies porta. 
+Donec tortor risus, rutrum vel enim sed, sodales eleifend ipsum. Suspendisse por
+ttitor tincidunt finibus. Donec ultricies 
+scelerisque arcu. Fusce non mauris non sapien feugiat mattis et eu eros. Class a
+ptent taciti sociosqu ad litora torquent per 
+conubia nostra, per inceptos himenaeos. Sed tristique pretium tellus, et volutpa
+t odio iaculis in. Phasellus vel libero nisi. 
+Sed facilisis pharetra libero quis varius.
+
+Quisque dictum blandit arcu luctus consequat. Donec iaculis vehicula tincidunt. 
+Vestibulum ante ipsum primis in faucibus orci 
+luctus et ultrices posuere cubilia curae; Integer pharetra sollicitudin lorem a 
+sodales. Morbi sit amet fringilla diam. In ut 
+dui non massa dignissim hendrerit. Nam pretium iaculis ipsum nec luctus. In bibe
+ndum sagittis gravida. Integer augue nibh, ultricies 
+a turpis ut, luctus vulputate mi. Vestibulum eget mauris elementum, molestie tur
+pis sagittis, varius ipsum. Sed nec dictum purus. 
+Sed imperdiet massa eget neque dictum, sit amet dignissim metus tristique. Sed a
+ tortor in nunc molestie rutrum a at mi. Proin 
+non nisl vitae ligula ullamcorper condimentum in ut nibh.
+
+In at massa non lacus pulvinar scelerisque at consequat nibh. Mauris molestie di
+gnissim aliquet. Pellentesque consequat, velit 
+et commodo molestie, augue sem pharetra metus, a porta turpis leo vel ipsum. Eti
+am fringilla urna et commodo hendrerit. Proin 
+lacinia mauris pretium faucibus volutpat. Fusce in tellus tempus, pulvinar lacus
+ eget, lobortis risus. Vivamus sit amet magna 
+nec nibh pretium maximus. Suspendisse ultricies sapien nec orci maximus sagittis
+. Sed sodales turpis ipsum, at dictum est vestibulum 
+ut. Mauris volutpat eros ante, eleifend mattis odio iaculis vel. Aenean sed ligu
+la pellentesque, suscipit sapien a, vehicula dui. 
+Proin accumsan lobortis arcu, vel congue nibh.
+
+Pellentesque congue malesuada congue. Nam rutrum condimentum rutrum. Morbi rutru
+m in purus in fringilla. Morbi tristique porta 
+arcu, et tempor nisi consectetur sed. Sed commodo ipsum sodales velit porta dign
+issim. Donec ut diam vitae libero egestas 
+ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+ per inceptos himenaeos. Aenean ultrices risus 
+risus, id fermentum lectus condimentum sit amet. Mauris aliquam ligula sed males
+uada vestibulum. Nunc blandit, sapien ut mollis 
+dignissim, velit nisl vehicula tellus, a fermentum est nunc ac ante. Nulla dolor
+ orci, posuere a enim id, rutrum placerat neque. 
+Nam a finibus nisi, vel lobortis mauris. Quisque vel lacus massa. Suspendisse co
+nvallis, justo et finibus efficitur, turpis arcu 
+pulvinar leo, cursus lacinia sapien diam vel sem. Nam euismod commodo metus vita
+e venenatis. Aenean tempus mattis porttitor.
+
+Integer id est eget magna porta ornare id vel odio. Etiam tincidunt libero nec l
+igula ornare tempor. Duis tellus diam, mollis sit 
+amet massa vel, dapibus euismod est. Cras eleifend condimentum orci quis vulputa
+te. Suspendisse pretium, neque sit amet vestibulum 
+maximus, erat metus interdum lacus, a efficitur est nulla at orci. Aenean semper
+ augue a commodo hendrerit. Nam ut justo posuere, 
+pellentesque massa at, tempor leo. Phasellus condimentum nunc quis est pulvinar,
+ a ornare erat vehicula. Nullam tempor tincidunt 
+dui vel dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+ in tortor erat. Vivamus vestibulum mi vel rutrum 
+scelerisque. Quisque feugiat, enim eget sollicitudin suscipit, lectus augue auct
+or quam, et iaculis ante lorem ac velit. Fusce venenatis 
+lectus sed dui congue facilisis. Sed porttitor est vel eros ornare porta.
+
+
+
+
+"""
