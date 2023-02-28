749c7a16c1441323c99ab0e5ac51045c113e7ecb
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-02-28 18:15:39 -0300

    more

--- AULA_33_Exercícios.py
+++ AULA_33_Exercícios.py
@@ -4,7 +4,9 @@ Criar um sistema bancário (extremamente simples) que tem clientes, contas e
 um banco. A ideia é que o cliente tenha uma conta (poupança ou corrente) e que
 possa sacar/depositar nessa conta. Contas corrente tem um limite extra.
 """
-class Conta():
+from abc import ABC, abstractmethod
+
+class Conta(ABC):
     def __init__(self, agencia, numconta, saldo) -> None:
         self._agencia = agencia
         self._numconta = numconta
@@ -13,12 +15,24 @@ class Conta():
     def deposito(self, dep):
         self._saldo += dep
 
+    @abstractmethod
+    def sacar(self): ...
+
 class ContaCorrente(Conta):
-    ...
+    def __init__(self, agencia, numconta, saldo) -> None:
+        super().__init__(agencia, numconta, saldo)
+
+    def sacar(self, valor):
+        print(f'Sacando R${valor}')
+        self._saldo -= valor
 
 class ContaPoupanca(Conta):
-    ...
-
+    def __init__(self, agencia, numconta, saldo) -> None:
+        super().__init__(agencia, numconta, saldo)
+    
+    def sacar(self, valor):
+        print(f'Sacando R${valor}')
+        self._saldo -= valor
 class Pessoa():
     def __init__(self, nome, idade) -> None:
         self._nome = nome
@@ -41,12 +55,10 @@ class Pessoa():
         self._nome = nome
 
 class Cliente(Pessoa):
-    def __init__(self) -> None:
-        self.conta = ...
+    def __init__(self, nome, idade) -> None:
+        super().__init__(nome, idade)
 
 class Banco():
     def __init__(self) -> None:
         self._contas = []
         self._clientes = []
-
-
