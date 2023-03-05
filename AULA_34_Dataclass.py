b3465718e7c5297fd45bc50bde1b7a4ec8f29cb7
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-03-05 10:09:50 -0300

    more

--- AULA_34_Dataclass.py
+++ AULA_34_Dataclass.py
@@ -13,6 +13,9 @@ class Pessoa:
     nome: str
     sobrenome: str
 
+    def __post_init__(self):
+        self.nome_completo = f'{self.nome}, {self.sobrenome}'
+
     @property
     def nome_completo(self):
         return f'{self.nome} {self.sobrenome}'
@@ -26,6 +29,4 @@ class Pessoa:
 
 if __name__ == '__main__':
     p1 = Pessoa('Luiz', 'Otávio')
-    p1.nome_completo = 'Maria Helena'
-    print(p1)
     print(p1.nome_completo)
